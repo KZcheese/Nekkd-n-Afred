2d0bf69be3b26cd3078da9b1cfb912db310ba242
Kevin Zhan <kevizha@amazon.com>
2018-08-03 15:54:30 -0700

    update jvm arguements

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -Xmx4096M -Xms1024M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar server.jar nogui
+java -Xmx4G -Xmn1G -Xms1G -XX:+UseG1GC -XX:+AggressiveOpts -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar server.jar nogui
+
